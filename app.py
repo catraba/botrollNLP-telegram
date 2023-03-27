1ad33627906b50b8d6f9b8ce12a6fd49cb027bd1
catraba <adriancarabantes@gmail.com>
2023-03-27 16:08:42 +0200

    connect_to_s3

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, request
 from messenger import messenger
 from Telegram import Telegram
 from unshared import TELEGRAM_TOKEN
+from utils.utils import write_response_to_csv
 
 
 app = Flask(__name__)
@@ -15,10 +16,17 @@ def main() -> None:
 
     if data['message']['chat']['type'] in ['group', 'supergroup']:
         message = data['message'].get('text')
+        reply_too_message = data['message'].get('reply_too_message')
 
         if isinstance(message, str):
             bot.send_message(data['message']['chat']['id'],
                             messenger(message),
                             data['message']['message_id'])
+
+        if reply_too_message:
+            write_response_to_csv(
+                reply_too_message['text'],
+                message
+            )
 
     return ''
